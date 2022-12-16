4e2c5c7b78495e8c3f28de305fe971ac96aaec79
Thorsten Hell <thorsten.hell@cismet.de>
2022-12-16 10:41:18 +0100

    classification with flat polygon

---
+++ src/test.js
@@ -0,0 +1,35 @@
+import react from "react";
+const x = () => {
+  return;
+  <div class='leaflet-control-container'>
+    <div class='leaflet-top leaflet-left'>
+      <div class='leaflet-control-zoom leaflet-bar leaflet-control'>
+        <a
+          class='leaflet-control-zoom-in'
+          href='#'
+          title='Vergrößern'
+          role='button'
+          aria-label='Vergrößern'
+        >
+          +
+        </a>
+        <a
+          class='leaflet-control-zoom-out'
+          href='#'
+          title='Verkleinern'
+          role='button'
+          aria-label='Verkleinern'
+        >
+          −
+        </a>
+      </div>
+      <div class='leaflet-control-fullscreen leaflet-bar leaflet-control'>
+        <a
+          class='leaflet-control-fullscreen-button leaflet-bar-part'
+          href='#'
+          title='Vollbildmodus'
+        ></a>
+      </div>
+    </div>
+  </div>;
+};
